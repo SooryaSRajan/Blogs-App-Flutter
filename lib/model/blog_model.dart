67b7af635170175f9dbd6fa5b49d5dce6b40c31a
sooryasrajan <soorya.s27@gmail.com>
2022-06-22 12:21:21 +0530

    Added blog model class

---
+++ lib/model/blog_model.dart
@@ -0,0 +1,8 @@
+class BlogModel {
+  String? authorName;
+  String? blogTitle;
+  String? blogContent;
+  String? date;
+
+  BlogModel({this.date, this.authorName, this.blogContent, this.blogTitle});
+}
