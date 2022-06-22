aee77f72c1a3b50e8af3e9a7603274518bfbc43d
sooryasrajan <soorya.s27@gmail.com>
2022-06-22 12:25:42 +0530

    Added JSON parsers for BlogModel

--- lib/model/blog_model.dart
+++ lib/model/blog_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class BlogModel {
   String? authorName;
   String? blogTitle;
@@ -5,4 +7,21 @@ class BlogModel {
   String? date;
 
   BlogModel({this.date, this.authorName, this.blogContent, this.blogTitle});
+
+  String toJSON() {
+    return json.encode({
+      "blog": blogContent,
+      "authorName": authorName,
+      "blogTitle": blogTitle
+    });
+  }
+
+  BlogModel fromJSON(String data) {
+    var decodedData = json.decode(data);
+    return BlogModel(
+        date: decodedData['date'],
+        authorName: decodedData['authorName'],
+        blogContent: decodedData['blog'],
+        blogTitle: decodedData['blogTitle']);
+  }
 }
